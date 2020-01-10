ae38a802afc8843f4bc4362745a3cdf6afa89aca
William Bogans <william.bogans@williambogans.dev>
2020-01-10 12:56:54 -0800

    doing something

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
+import Select from 'react-select';
 const traitJson = require('./traits.json');
 
 function App() {
-  const [synergy, setSynergy] = useState(0);
+  const [synergyCount, setSynergyCount] = useState(0);
+  const [choice, setChoice] = useState({});
+  const [team, setTeam] = useState();
+
+  function handleChange(e, name) {
+    setSynergyCount(synergyCount + 1);
+    //If synergy count is greater than 9? Do something
+    setChoice(prevState => ({ ...prevState, [name]: e }));
+    console.log(choice);
+  }
 
   function renderTraitOptions() {
     return traitJson.map(trait => {
@@ -12,12 +22,16 @@ function App() {
         <div className="traitOption">
           <label htmlFor="traitName">
             {trait.name}
-            <select name="name?" id="cool">
-              <option value="">Please choose a number</option>
-              {numOfTraits.map(num => (
-                <option value={`${num}`}>{num}</option>
-              ))}
-            </select>
+            <Select
+              value={trait.name}
+              options={trait.sets}
+              onChange={e => handleChange(e, trait.name)}
+              placeholder="Please choose a number"
+            />
+            <option value="">Please choose a number</option>
+            {numOfTraits.map(num => (
+              <option value={`${num}`}>{num}</option>
+            ))}
           </label>
         </div>
       );
@@ -28,9 +42,11 @@ function App() {
     <div className="App">
       <section className="hero-section">
         <h1>Team Fighter Tactics Composition Builder</h1>
-        <h3>Total Synergys: {synergy}</h3>
+        <h3>Total Synergys: {synergyCount}</h3>
         <section className="trait-section">{renderTraitOptions()}</section>
       </section>
+
+      <button onClick={() => console.log('hey')}>Get Team</button>
 
       <section className="team-display">
         <h2>Your Team</h2>
