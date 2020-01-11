3da19604d5a809515ae9919be247e4edc703afff
William Bogans <william.bogans@williambogans.dev>
2020-01-10 16:50:12 -0800

    deploy

---
+++ build/precache-manifest.a53b2662f4488725ccc68c7c4ff6ef19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a5baeb7cec673f32790d08c473e3bb",
+    "url": "/TFTComps/index.html"
+  },
+  {
+    "revision": "0f1f094a21475b632d04",
+    "url": "/TFTComps/static/css/2.79966c17.chunk.css"
+  },
+  {
+    "revision": "1b9087aa81a1e27aaddf",
+    "url": "/TFTComps/static/css/main.acc47d5c.chunk.css"
+  },
+  {
+    "revision": "0f1f094a21475b632d04",
+    "url": "/TFTComps/static/js/2.16b23d0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TFTComps/static/js/2.16b23d0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b9087aa81a1e27aaddf",
+    "url": "/TFTComps/static/js/main.9d40ec46.chunk.js"
+  },
+  {
+    "revision": "61905e86fcfed5bea9dd",
+    "url": "/TFTComps/static/js/runtime-main.9897b6ae.js"
+  }
+]);
